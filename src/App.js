728cf24205d112f7d3c2732ead80ba0fadda9df8
KGrabowski <Kamil.Grabowski@hemmersbach.com>
2019-01-14 15:52:21 +0100

    first change

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Elo, elo 320
           </a>
         </header>
       </div>
