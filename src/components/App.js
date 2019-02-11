9ce4713008b48385adc947f4d967a554ce31194b
kamil <kamil_grabowski@vp.pl>
2019-02-11 18:44:18 +0100

    AppBar first step

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import Button from "@material-ui/core/Button";
-import Cardd from './Card';
-
+import React, { Component } from 'react';
+import HeaderOfSide from './HeaderOfSide';
 
 class App extends React.Component {
+
+
     render() {
         return (
             <div>
-               <h4>Hello World</h4>
-                <Button variant="contained"
-                        color="primary">
-                    Hello World
-                </Button>
-                <Cardd/>
+                <div>
+                 <HeaderOfSide/>
+                </div>
+                <div>
+                    Siemanko
+                </div>
             </div>
         );
     }
